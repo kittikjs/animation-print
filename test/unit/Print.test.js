52738839cf2cca16306e3653d5daad40754eae66
ghaiklor <ghaiklor@gmail.com>
2015-12-08 14:01:46 +0200

    test(animation): Add missing tests

--- test/unit/Print.test.js
+++ test/unit/Print.test.js
@@ -6,24 +6,38 @@ describe('Animation::Print', () => {
   it('Should properly accept options object', () => {
     let animation = new Print({interval: 500});
 
-    assert.equal(animation._interval, 500);
+    assert.equal(animation.get('interval'), 500);
   });
 
-  it('Should properly get interval', () => {
+  it('Should properly get/set interval', () => {
     let animation = new Print();
 
     assert.equal(animation.getInterval(), 100);
-  });
-
-  it('Should properly set interval', () => {
-    let animation = new Print();
-
     assert.instanceOf(animation.setInterval(1000), Print);
     assert.equal(animation.getInterval(), 1000);
   });
 
-  it('Should properly call the animate() method', () => {
+  it('Should properly check if animation is with random interval', () => {
     let animation = new Print();
+
+    assert.ok(animation.isRandom());
+    assert.instanceOf(animation.setRandom(false), Print);
+    assert.notOk(animation.isRandom());
+  });
+
+  it('Should properly call the animate() method with random option', () => {
+    let animation = new Print();
+    let cb = sinon.spy();
+
+    animation.animate('test', cb);
+
+    setTimeout(() => {
+      assert.equal(cb.getCall(0).args[0], 'test');
+    }, 200);
+  });
+
+  it('Should properly call the animate() method without random option', () => {
+    let animation = new Print({random: false});
     let cb = sinon.spy();
 
     animation.animate('test', cb);
