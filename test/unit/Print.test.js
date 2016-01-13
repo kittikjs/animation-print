9b194ed307fd8ef13454c591d54b622d581b0e75
ghaiklor <ghaiklor@gmail.com>
2016-01-13 16:19:11 +0200

    test(animation): Fix tests

--- test/unit/Print.test.js
+++ test/unit/Print.test.js
@@ -1,49 +1,32 @@
 import { assert } from 'chai';
 import sinon from 'sinon';
+import Rectangle from 'kittik-shape-rectangle';
 import Print from '../../src/Print';
 
 describe('Animation::Print', () => {
-  it('Should properly accept options object', () => {
-    let animation = new Print({interval: 500});
-
-    assert.equal(animation.get('interval'), 500);
-  });
-
-  it('Should properly get/set interval', () => {
+  it('Should properly call the animate() method', done => {
     let animation = new Print();
+    let shape = new Rectangle();
+    let mock = sinon.mock(animation);
 
-    assert.equal(animation.getInterval(), 100);
-    assert.instanceOf(animation.setInterval(1000), Print);
-    assert.equal(animation.getInterval(), 1000);
+    mock.expects('animateProperty').once().returns(Promise.resolve());
+
+    animation.animate(shape).then(() => {
+      mock.verify();
+      done();
+    });
   });
 
-  it('Should properly check if animation is with random interval', () => {
-    let animation = new Print();
+  it('Should properly animate property', done => {
+    let animation = new Print({duration: 1});
+    let shape = new Rectangle();
+    let mock = sinon.mock(animation);
 
-    assert.ok(animation.isRandom());
-    assert.instanceOf(animation.setRandom(false), Print);
-    assert.notOk(animation.isRandom());
-  });
+    mock.expects('emit').atLeast(1).withArgs('tick');
 
-  it('Should properly call the animate() method with random option', () => {
-    let animation = new Print();
-    let cb = sinon.spy();
-
-    animation.animate('test', cb);
-
-    setTimeout(() => {
-      assert.equal(cb.getCall(0).args[0], 'test');
-    }, 200);
-  });
-
-  it('Should properly call the animate() method without random option', () => {
-    let animation = new Print({random: false});
-    let cb = sinon.spy();
-
-    animation.animate('test', cb);
-
-    setTimeout(() => {
-      assert.equal(cb.getCall(0).args[0], 'test');
-    }, 200);
+    animation.animate(shape).then(() => {
+      mock.verify();
+      done();
+    });
   });
 });
