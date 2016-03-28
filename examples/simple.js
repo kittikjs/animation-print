6066996fd7037242aa186e80034579c9244eb8ca
ghaiklor <ghaiklor@gmail.com>
2016-03-28 15:42:38 +0300

    docs(animation): Update examples

--- examples/simple.js
+++ examples/simple.js
@@ -1,11 +1,11 @@
 "use strict";
 
-const cursor = require('kittik-cursor').create().resetTTY().hideCursor();
+const cursor = require('kittik-cursor').create().reset().hideCursor();
 const Print = require('../lib/Print');
-const shape = require('kittik-shape-rectangle').create({
+const shape = require('kittik-shape-rectangle').create(cursor, {
   text: 'Good news, everybody!',
   x: 'center',
-  background: 'yellow_1',
+  background: 'yellow',
   foreground: 'black',
   width: '50%'
 });
@@ -13,4 +13,4 @@ const shape = require('kittik-shape-rectangle').create({
 new Print({
   easing: 'inOutSine',
   duration: 5000
-}).on('tick', shape => shape.render(cursor) && cursor.flush().eraseScreen()).animate(shape).then(() => cursor.showCursor().flush());
+}).on('tick', shape => cursor.eraseScreen() && shape.render() && cursor.flush()).animate(shape).then(() => cursor.showCursor());
